60e6aa844f61238d934875b0821ce8d92d80a037
Eric Ahnell <eric.ahnell@puttysoftware.com>
2017-07-07 11:44:14 -0400

    Add file I/O classes.

---
+++ DynamicDungeon/src/net/dynamicdungeon/fileio/XMLFileConstants.java
@@ -0,0 +1,11 @@
+package net.dynamicdungeon.fileio;
+
+class XMLFileConstants {
+    public static final String X_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    public static final String DOUBLE_TAG = "double";
+    public static final String INT_TAG = "integer";
+    public static final String LONG_TAG = "long";
+    public static final String BYTE_TAG = "byte";
+    public static final String BOOLEAN_TAG = "boolean";
+    public static final String STRING_TAG = "string";
+}
---
+++ DynamicDungeon/src/net/dynamicdungeon/fileio/XMLFileWriter.java
@@ -0,0 +1,95 @@
+package net.dynamicdungeon.fileio;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class XMLFileWriter implements AutoCloseable {
+    // Fields
+    private BufferedWriter bw;
+    private String docTag;
+    private static final String END_OF_LINE = "\r\n";
+
+    // Constructors
+    public XMLFileWriter(String filename, String newDocTag) throws IOException {
+        this.bw = new BufferedWriter(new FileWriter(filename));
+        this.docTag = newDocTag;
+        this.writeXHeader();
+        this.writeOpeningDocTag();
+    }
+
+    // Methods
+    @Override
+    public void close() throws IOException {
+        this.writeClosingDocTag();
+        this.bw.close();
+    }
+
+    public void writeDouble(double d) throws IOException {
+        this.bw.write("<" + XMLFileConstants.DOUBLE_TAG + ">"
+                + Double.toString(d) + "</" + XMLFileConstants.DOUBLE_TAG + ">"
+                + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeInt(int i) throws IOException {
+        this.bw.write("<" + XMLFileConstants.INT_TAG + ">" + Integer.toString(i)
+                + "</" + XMLFileConstants.INT_TAG + ">" + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeLong(long l) throws IOException {
+        this.bw.write("<" + XMLFileConstants.LONG_TAG + ">" + Long.toString(l)
+                + "</" + XMLFileConstants.LONG_TAG + ">"
+                + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeByte(byte b) throws IOException {
+        this.bw.write("<" + XMLFileConstants.BYTE_TAG + ">" + Byte.toString(b)
+                + "</" + XMLFileConstants.BYTE_TAG + ">"
+                + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeBoolean(boolean b) throws IOException {
+        this.bw.write("<" + XMLFileConstants.BOOLEAN_TAG + ">"
+                + Boolean.toString(b) + "</" + XMLFileConstants.BOOLEAN_TAG + ">"
+                + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeString(String s) throws IOException {
+        this.bw.write("<" + XMLFileConstants.STRING_TAG + ">"
+                + XMLFileWriter.replaceSpecialCharacters(s) + "</"
+                + XMLFileConstants.STRING_TAG + ">" + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeOpeningGroup(String groupName) throws IOException {
+        this.bw.write("<" + XMLFileWriter.replaceSpecialCharacters(groupName)
+                + ">" + XMLFileWriter.END_OF_LINE);
+    }
+
+    public void writeClosingGroup(String groupName) throws IOException {
+        this.bw.write("</" + XMLFileWriter.replaceSpecialCharacters(groupName)
+                + ">" + XMLFileWriter.END_OF_LINE);
+    }
+
+    private void writeXHeader() throws IOException {
+        this.bw.write(XMLFileConstants.X_HEADER + XMLFileWriter.END_OF_LINE);
+    }
+
+    private void writeOpeningDocTag() throws IOException {
+        this.bw.write("<" + this.docTag + ">" + XMLFileWriter.END_OF_LINE);
+    }
+
+    private void writeClosingDocTag() throws IOException {
+        this.bw.write("</" + this.docTag + ">");
+    }
+
+    private static String replaceSpecialCharacters(String s) {
+        String r = s;
+        r = r.replace("&", "&amp;");
+        r = r.replace("<", "&lt;");
+        r = r.replace(">", "&gt;");
+        r = r.replace("\"", "&quot;");
+        r = r.replace("\'", "&apos;");
+        r = r.replace("\r", "");
+        return r.replace("\n", "&#xA;");
+    }
+}
